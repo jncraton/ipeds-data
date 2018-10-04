3b26e22c18e7789cfab266c0738c16bede589916
Jon Craton <jncraton@gmail.com>
2018-10-03 22:27:40 -0400

    Code cleanup and more financials

--- gen_comparison.py
+++ gen_comparison.py
@@ -6,6 +6,22 @@ columns = [
   ('UAGRNTA','sfa{ay}.csv','Average Total Aid'),
   ('UPGRNTA','sfa{ay}.csv','Average Pell Grant Aid'),
   ('IGRNT_A','sfa{ay}.csv','Average Institutional Grant Aid'),
+  ('F2A02','f1516_f2.csv','Total Assets'),
+  ('F2A03','f1516_f2.csv','Total Liabilities'),
+  ('F2A03A','f1516_f2.csv','Debt related to Property, Plant, and Equipment'),
+  ('F2A17','f1516_f2.csv','Total Plant, Property, and Equipment'),
+  ('F2A18','f1516_f2.csv','Accumulated Depreciation'),
+  ('F2B01','f1516_f2.csv','Total Revenue'),
+  ('F2B02','f1516_f2.csv','Total Expenses'),
+  ('F2B04','f1516_f2.csv','Total change in net assets'),
+  ('F2C05','f1516_f2.csv','Institutional Grants (funded)'),
+  ('F2C06','f1516_f2.csv','Institutional Grants (unfunded)'),
+  ('F2C07','f1516_f2.csv','Total Student Grants'),
+  ('F2C10','f1516_f2.csv','Total Allowances and Discounts'),
+  ('F2D01','f1516_f2.csv','Total Tuition and Fees'),
+  ('F2D08','f1516_f2.csv','Private gifts and grants'),
+  ('F2D10','f1516_f2.csv','Investment return'),
+  ('F2D16','f1516_f2.csv','Total Revenue and Investment return'),
 ]
 
 schools = {}
@@ -22,30 +38,30 @@ for y in [2000 + i for i in range(10,18)]:
         except KeyError:
           schools[r['UNITID']][c] = ''
 
-for c in columns:
-  for y in [2000 + i for i in range(10,18)]:
-    if '{y}' in c[1]:
-      print(c[0] + y)
-      final_columns.append(c[0] + y)
-      with open('data/hd%s.csv' % str(y), encoding = 'cp1252') as f:
-        for r in csv.DictReader(f):
-          schools[r['UNITID']][c[0] + y] = r[c[0]]
+def append_column(filename, col, ver, desc):
+  name = "%s %s (%s)" % (col, ver, desc)
+  print(name)
+  final_columns.append(name)
+  with open('data/' + filename, encoding = 'cp1252') as f:
+    for r in csv.DictReader(f):
+      try:
+        schools[r['UNITID']][name] = r[col]
+      except KeyError:
+        try:
+          schools[r['UNITID']][name] = ''
+        except KeyError:
+          # School didn't exist in imported list
+          pass
 
 for c in columns:
-  for ay in ['0708', '0809', '0910', '1011', '1112', '1213', '1314', '1415', '1516']:
-    if '{ay}' in c[1]:
-      print(c[0] + ay)
-      final_columns.append(c[0] + ay)
-      with open('data/' + c[1].replace('{ay}',ay)) as d:
-        for r in csv.DictReader(d):
-          try:
-            schools[r['UNITID']][c[0] + ay] = r[c[0]]
-          except KeyError:
-            try:
-              schools[r['UNITID']][c[0] + ay] = ''            
-            except KeyError:
-              # School didn't exist in imported list
-              pass
+  if '{y}' in c[1]:
+    for y in [2000 + i for i in range(10,18)]:
+      append_column(c[1].replace('{y}',y),c[0],y,c[2])
+  elif '{ay}' in c[1]:
+    for ay in ['0708', '0809', '0910', '1011', '1112', '1213', '1314', '1415', '1516']:
+      append_column(c[1].replace('{ay}',ay),c[0],ay,c[2])
+  else:
+    append_column(c[1],c[0],'latest',c[2])
 
 with open('data/comparison.tsv', 'w') as f:
   writer = csv.DictWriter(f, dialect='excel-tab', fieldnames=final_columns)
